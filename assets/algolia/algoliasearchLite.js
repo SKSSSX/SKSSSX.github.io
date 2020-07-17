8f2c830db8e11792d236b5eac844a68b5b1ee1e4
tsl-shengkesi <tsl-shengkesi@tasly.com>
2020-07-17 10:46:03 +0800

    Site updated: 2020-07-17 10:46:00

--- assets/algolia/algoliasearchLite.js
+++ assets/algolia/algoliasearchLite.js
@@ -1,4 +1,4 @@
-/*! algoliasearch 3.35.1 | © 2014, 2015 Algolia SAS | github.com/algolia/algoliasearch-client-js */
+/*! algoliasearch 3.31.0 | © 2014, 2015 Algolia SAS | github.com/algolia/algoliasearch-client-js */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.algoliasearch = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (process){
 /**
@@ -2477,10 +2477,8 @@ AlgoliaSearchCore.prototype.unsetExtraHeader = function(name) {
 * @param algoliaAgent the agent to add
 */
 AlgoliaSearchCore.prototype.addAlgoliaAgent = function(algoliaAgent) {
-  var algoliaAgentWithDelimiter = '; ' + algoliaAgent;
-
-  if (this._ua.indexOf(algoliaAgentWithDelimiter) === -1) {
-    this._ua += algoliaAgentWithDelimiter;
+  if (this._ua.indexOf(';' + algoliaAgent) === -1) {
+    this._ua += ';' + algoliaAgent;
   }
 };
 
@@ -2737,7 +2735,7 @@ AlgoliaSearchCore.prototype._jsonRequest = function(initialOpts) {
         return client._promise.reject(err);
       }
 
-      // When a timeout occurred, retry by raising timeout
+      // When a timeout occured, retry by raising timeout
       if (err instanceof errors.RequestTimeout) {
         return retryRequestWithHigherTimeout();
       }
@@ -2876,7 +2874,7 @@ AlgoliaSearchCore.prototype._computeRequestHeaders = function(options) {
   var forEach = require(4);
 
   var ua = options.additionalUA ?
-    this._ua + '; ' + options.additionalUA :
+    this._ua + ';' + options.additionalUA :
     this._ua;
 
   var requestHeaders = {
@@ -3585,7 +3583,7 @@ IndexCore.prototype._search = function(params, url, callback, additionalUA) {
 * @param attrs (optional) if set, contains the array of attribute names to retrieve
 * @param callback (optional) the result callback called with two arguments
 *  error: null or Error('message')
-*  content: the object to retrieve or the error message if a failure occurred
+*  content: the object to retrieve or the error message if a failure occured
 */
 IndexCore.prototype.getObject = function(objectID, attrs, callback) {
   var indexObj = this;
@@ -3671,7 +3669,7 @@ IndexCore.prototype.typeAheadValueOption = null;
 var AlgoliaSearchCore = require(15);
 var createAlgoliasearch = require(18);
 
-module.exports = createAlgoliasearch(AlgoliaSearchCore, 'Browser (lite)');
+module.exports = createAlgoliasearch(AlgoliaSearchCore, '(lite) ');
 
 },{"15":15,"18":18}],18:[function(require,module,exports){
 (function (process){
@@ -3706,10 +3704,7 @@ module.exports = function createAlgoliasearch(AlgoliaSearch, uaSuffix) {
   }
 
   algoliasearch.version = require(32);
-
-  algoliasearch.ua =
-    'Algolia for JavaScript (' + algoliasearch.version + '); ' + uaSuffix;
-
+  algoliasearch.ua = 'Algolia for vanilla JavaScript ' + uaSuffix + algoliasearch.version;
   algoliasearch.initPlaces = places(algoliasearch);
 
   // we expose into window no matter how we are used, this will allow
@@ -4218,7 +4213,7 @@ module.exports = {
   ),
   RequestTimeout: createCustomError(
     'RequestTimeout',
-    'Request timed out before getting a response'
+    'Request timedout before getting a response'
   ),
   Network: createCustomError(
     'Network',
@@ -4228,17 +4223,9 @@ module.exports = {
     'JSONPScriptFail',
     '<script> was loaded but did not call our provided callback'
   ),
-  ValidUntilNotFound: createCustomError(
-    'ValidUntilNotFound',
-    'The SecuredAPIKey does not have a validUntil parameter.'
-  ),
   JSONPScriptError: createCustomError(
     'JSONPScriptError',
     '<script> unable to load due to an `error` event on it'
-  ),
-  ObjectNotFound: createCustomError(
-    'ObjectNotFound',
-    'Object not found'
   ),
   Unknown: createCustomError(
     'Unknown',
@@ -4447,7 +4434,7 @@ function cleanup() {
 },{"1":1}],32:[function(require,module,exports){
 'use strict';
 
-module.exports = '3.35.1';
+module.exports = '3.31.0';
 
 },{}]},{},[17])(17)
 });
